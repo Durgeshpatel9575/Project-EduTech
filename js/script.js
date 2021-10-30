6c1f6c53bf93fb13f3dc814e543fdaf0ca0e5ad4
Durgesh Patel <pateldurgesh9575@gmail.com>
2021-10-30 23:50:06 +0530

    first commit

---
+++ js/script.js
@@ -0,0 +1,74 @@
+document.getElementById("h-one").addEventListener("click", function() {
+    window.scrollTo({
+        top: 1,
+        left: 100,
+        behavior: 'smooth'
+    })
+})
+document.getElementById("h-two").addEventListener("click", function() {
+    window.scrollTo({
+        top: 1000,
+        left: 100,
+        behavior: 'smooth'
+    })
+})
+
+document.getElementById("h-three").addEventListener("click", function() {
+    window.scrollTo({
+        top: 2000,
+        left: 100,
+        behavior: 'smooth'
+    })
+})
+
+document.getElementById("h-four").addEventListener("click", function() {
+    window.scrollTo({
+        top: 3000,
+        left: 100,
+        behavior: 'smooth'
+    })
+})
+document.getElementById("h-five").addEventListener("click", function() {
+    window.scrollTo({
+        top: 4000,
+        left: 100,
+        behavior: 'smooth'
+    })
+})
+document.getElementById("h-six").addEventListener("click", function() {
+    window.scrollTo({
+        top: 5000,
+        left: 100,
+        behavior: 'smooth'
+    })
+})
+document.getElementById("h-seven").addEventListener("click", function() {
+    window.scrollTo({
+        top: 5000,
+        left: 100,
+        behavior: 'smooth'
+    })
+})
+document.getElementById("h-eight").addEventListener("click", function() {
+    window.scrollTo({
+        top: 6000,
+        left: 100,
+        behavior: 'smooth'
+    })
+})
+document.getElementById("h-nine").addEventListener("click", function() {
+    window.scrollTo({
+        top: 7000,
+        left: 100,
+        behavior: 'smooth'
+    })
+})
+document.getElementById("h-ten").addEventListener("click", function() {
+    window.scrollTo({
+        top: 8000,
+        left: 100,
+        behavior: 'smooth'
+    })
+})
+
+// css page
